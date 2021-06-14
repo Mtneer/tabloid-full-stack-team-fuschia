9d39d1c0c37bd4d36229d92206d0e78fd4801f4d
David Billups <davidtbillups@gmail.com>
2021-06-14 11:36:32 -0400

    still need to determine how to display data for post to be edited

--- Tabloid/client/src/components/PostForm.js
+++ Tabloid/client/src/components/PostForm.js
@@ -43,7 +43,7 @@ export const PostForm = () => {
     }
 
     const handleClickSavePost = (event) => {
-        event.preventDefault() //prevents the browser from submitting the form
+        // event.preventDefault() //prevents the browser from submitting the form
 
         // function extractId() {
         //     var str = loggedInUserId;
@@ -79,59 +79,58 @@ export const PostForm = () => {
         }
     }
 
-return (
-    <form className="postForm">
-        <h2 className="postForm__title">New Post</h2>
-        <fieldset>
-            <div className="form-group">
-                <label htmlFor="title">Title:</label>
-                <input type="text" id="title" onChange={handleControlledInputChange} required autoFocus className="form-control" placeholder="post title" value={postFormInput?.title} />
+    return (
+        <form className="postForm">
+            <h2 className="postForm__title">New Post</h2>
+            <fieldset>
+                <div className="form-group">
+                    <label htmlFor="title">Title:</label>
+                    <input type="text" id="title" onChange={handleControlledInputChange} required autoFocus className="form-control" placeholder="post title" value={postFormInput?.title} />
+                </div>
+            </fieldset>
+            <fieldset>
+                <div className="form-group">
+                    <label htmlFor="content">Content:</label>
+                    <input type="text" id="content" onChange={handleControlledInputChange} required autoFocus className="form-control" placeholder="content" value={postFormInput?.content} />
+                </div>
+            </fieldset>
+            <fieldset className="col-6">
+                <label htmlFor="category">Category:</label>
+                <select
+                    value={postFormInput?.categoryId}
+                    name="category"
+                    id="categoryId"
+                    onChange={handleControlledInputChange}
+                    required
+                    className="form-control" >
+                    <option value="0">Select a category</option>
+                    {categories.map(currentCategory => (
+                        <option
+                            key={currentCategory?.id}
+                            value={currentCategory?.id}>
+                            {currentCategory?.name}
+                        </option>
+                    ))}
+                </select>
+            </fieldset>
+            <fieldset>
+                <div className="form-group">
+                    <label htmlFor="imageURL">Image URL:</label>
+                    <input type="text" id="imageLocation" onChange={handleControlledInputChange} autoFocus className="form-control" placeholder="image URL" value={postFormInput?.imageLocation} />
+                </div>
+            </fieldset>
+            <fieldset>
+                <div className="form-group">
+                    <label htmlFor="publication date">Publication Date:</label>
+                    <input type="datetime-local" id="publishDateTime" onChange={handleControlledInputChange} autoFocus className="form-control" placeholder="publication date" value={postFormInput?.publishDateTime} />
+                </div>
+            </fieldset>
+            <div className="button-container">
+                <button className="button btn btn-primary"
+                    onClick={handleClickSavePost} disable={isLoading.toString()}>
+                    {postId ? <>Save Post</> : <>Add Post</>}
+                </button>
             </div>
-        </fieldset>
-        <fieldset>
-            <div className="form-group">
-                <label htmlFor="content">Content:</label>
-                <input type="text" id="content" onChange={handleControlledInputChange} required autoFocus className="form-control" placeholder="content" value={postFormInput?.content} />
-            </div>
-        </fieldset>
-        <fieldset className="col-6">
-            <label htmlFor="category">Category:</label>
-            <select
-                value={postFormInput?.categoryId}
-                name="category"
-                id="categoryId"
-                onChange={handleControlledInputChange}
-                required
-                className="form-control" >
-                <option value="0">Select a category</option>
-                {categories.map(currentCategory => (
-                    <option
-                        key={currentCategory.id}
-                        value={currentCategory.id}>
-                        {currentCategory.name}
-                    </option>
-                ))}
-            </select>
-        </fieldset>
-        <fieldset>
-            <div className="form-group">
-                <label htmlFor="imageURL">Image URL:</label>
-                <input type="text" id="imageLocation" onChange={handleControlledInputChange} autoFocus className="form-control" placeholder="image URL" value={postFormInput?.imageLocation} />
-            </div>
-        </fieldset>
-        <fieldset>
-            <div className="form-group">
-                <label htmlFor="publication date">Publication Date:</label>
-                <input type="datetime-local" id="publishDateTime" onChange={handleControlledInputChange} autoFocus className="form-control" placeholder="publication date" value={postFormInput?.publishDateTime} />
-            </div>
-        </fieldset>
-        <div className="button-container">
-            <button className="button btn btn-primary"
-                onClick={handleClickSavePost} disable={isLoading}>
-                {postId ? <>Save Post</> : <>Add Post</>}
-            </button>
-        </div>
-    </form>
-)
-
+        </form>
+    )
 }
